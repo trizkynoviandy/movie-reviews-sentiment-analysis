63b8b068c3e133a99cc0b94eaeeae5ffcc63ba19
teukurizkyn <trizkynoviandy@gmail.com>
2022-05-11 21:09:06 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Movie Review Sentiment Analysis
+
+Dataset source: <http://ai.stanford.edu/~amaas/data/sentiment/>
---
+++ convert_dataset_to_dataframe.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import pandas as pd
+import pyprind
+
+basepath = 'aclImdb'
+
+labels = {'pos': 1, 'neg':0}
+
+pbar = pyprind.ProgBar(50000, stream=sys.stdout)
+df = pd.DataFrame()
+
+for s in ('test', 'train'):
+    for l in ('pos', 'neg'):
+        path = os.path.join(basepath, s, l)
+        for file in sorted(os.listdir(path)):
+            with open(os.path.join(path, file),
+                      'r', encoding='utf-8') as infile:
+                txt = infile.read()
+            df = df.append([[txt, labels[l]]], ignore_index=True)
+            pbar.update()
+            
+df.columns = ['review', 'sentiment']
+
+import numpy as np
+
+np.random.seed(0)
+df = df.reindex(np.random.permutation(df.index))
+df.to_csv('movie_data.csv', index=False, encoding='utf-8')
